5b5df468b208ac9747fe8eb7d7fbf520c6fb4cc3
JessicaHarveyCross <jessieharveycross@gmail.com>
2026-07-01 19:56:42 +1200

    setup cat soul manager script

---
+++ Café Purrgatory/Assets/Scripts/CatSoulManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CatSoulManager : MonoBehaviour
+{
+    //available cats
+    //energy??
+    //happiness??
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
