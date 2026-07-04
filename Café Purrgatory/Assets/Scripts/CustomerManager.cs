908893c75d3da210b474d1b930e329a95962f29c
JessicaHarveyCross <jessieharveycross@gmail.com>
2026-07-04 23:39:30 +1200

    added spawn customer and started table functionality

--- Café Purrgatory/Assets/Scripts/CustomerManager.cs
+++ Café Purrgatory/Assets/Scripts/CustomerManager.cs
@@ -6,16 +6,31 @@ public class CustomerManager : MonoBehaviour
     //queue
     //seating
 
+    public TableManager m_currentTable;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public CustomerSoul m_customerPrefab;
+
+    public Transform m_door;
+
+    public TableManager[] m_tables;
+
+    public void SpawnCustomer()
     {
-        
+        CustomerSoul customer = Instantiate(m_customerPrefab, m_door.position, Quaternion.identity);
+        foreach (TableManager table in m_tables)
+        {
+            if (!table.m_occupied)
+            {
+                SitAtTable(table);
+                return;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SitAtTable(TableManager _table)
     {
-        
+        m_currentTable = _table;
+        transform.position = _table.m_seatPoint.position;
+        _table.m_occupied = true;
     }
 }
